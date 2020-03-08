910caea753375c773a363bf2a4c1f896dc5ffe1a
Strudenjaden <48246921+Strudenjaden@users.noreply.github.com>
2020-03-09 00:16:42 +0100

    Player Movement and Camera v1
    Easy PlayerMovement and Camera Rotation v1.
    The camera's script dont work well yet.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Rigidbody rb;
+    [Header("Velocities")]
+    public float speed;
+    public float walkSpeed = 3;
+    public float runSpeed = 6;
+    public bool isRunning = false;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Movement();
+
+    }
+
+    void Movement()
+    {
+        //Making easy movement to the player.
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(moveH * speed, rb.velocity.y, moveV * speed);
+        Debug.Log(moveV);
+
+        //Making player run
+        if (Input.GetKey(KeyCode.LeftShift) && moveV >= 0)
+        {
+            speed = runSpeed;
+            isRunning = true;
+            Debug.Log("Running");
+        } else
+        {
+            speed = walkSpeed;
+            isRunning = false;
+            Debug.Log("Not running");
+        }
     }
 }
