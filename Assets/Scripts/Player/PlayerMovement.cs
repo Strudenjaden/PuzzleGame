fe60e663a389e26aa3e93fa9bc8abf25d8138fb0
Strudenjaden <48246921+Strudenjaden@users.noreply.github.com>
2020-03-10 21:42:46 +0100

    Improve on Movement
    Movement improved. Seraching how to make player jump correctly. Cam is doing this hard :(

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,48 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header("Objects")]
     public Rigidbody rb;
+    public Camera mainCamera;
+
     [Header("Movement")]
     public float speed;
-    private float walkSpeed = 5;
-    private float runSpeed = 10;
+    private float walkSpeed = 5f;
+    private float runSpeed = 8f;
+    private float backwardsSpeed = 2f;
     public bool isRunning = false;
     Vector3 movement = Vector3.zero;
+    float moveH, moveV; //Inputs
+    float jumpForce = 20f;
 
-    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+    }
+
+
     void FixedUpdate()
     {
-        Move();
 
+        Move();
+        Jump();
     }
 
     void Move()
     {
+        //Velocities
+        speed = walkSpeed; //The normal player velocity.
+        Backwards();
+        Run();
+        Debug.Log("Velocidad: " + speed);
+
+
         //Making easy movement to the player.
-        float moveH = Input.GetAxis("Horizontal");
-        float moveV = Input.GetAxis("Vertical");
-        movement = new Vector3(moveH * speed, rb.velocity.y, moveV * speed);
-
+        movement = new Vector3(moveH, rb.velocity.y, moveV);
         //Movement where the Main Camera is looking at;
-        movement = Camera.main.transform.TransformDirection(movement);
-        movement.y = 0.0f;
+        movement = mainCamera.transform.TransformDirection(movement);
+        //movement.y = 0.0f;
+        //Moving the rigidbody. Put this always at the end of the code to make the player walk.
+        rb.velocity = movement * speed;
+    }
 
-        //Moving the rigidbody.
-        rb.velocity = movement;
-        
-
+    void Run()
+    {
         //Making player run
         if (Input.GetKey(KeyCode.LeftShift) && moveV >= 0)
         {
             speed = runSpeed;
             isRunning = true;
             Debug.Log("Running");
-        } else
+        }else
+
+        //if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-            speed = walkSpeed;
             isRunning = false;
-            Debug.Log("Not running");
         }
+    }
+    void Backwards()
+    {
+        //Making player walking slower when is going backwards.
+        if (moveV < 0)
+        {
+            speed = backwardsSpeed;
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
